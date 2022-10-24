1cde3d959dde5c72fb4d0d03d2db9c18f9df3f85
bolognini <matheusbolognini79@gmail.com>
2022-10-24 17:56:45 +0200

    first commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const makeRelative = (files) =>
+  files.map((file) => path.relative(__dirname, file));
+
+const lintStagedConfig = {
+  '*.{js}': (files) => [
+    `prettier --write ${makeRelative(files).join(' ')}`,
+  ],
+  '*.json': ['prettier --write'],
+  '*.md': ['prettier --write'],
+  '*.{yml,yaml}': ['prettier --write'],
+};
+
+module.exports = lintStagedConfig;
